2f6421fffbd981989134d761145971aded9c718c
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-02-14 19:56:18 +0000

    adding API data

--- react-weather-app/src/components/weather/weather.jsx
+++ react-weather-app/src/components/weather/weather.jsx
@@ -1,31 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../search/search";
 
 export default function Weather() {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
-  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [weatherData, setWeatherData] = useState("");
 
-  async function handleSearch() {
+  async function fetchWeatherData(input) {
+    setLoading(true);
     try {
-      setLoading(true);
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={10d1e3574387d2bf7d483a5c4a78b1d6}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=10d1e3574387d2bf7d483a5c4a78b1d6`
       );
       const data = await response.json();
+      console.log(data);
+
+      if (data) setLoading(false), setWeatherData(data);
     } catch (error) {
+      console.log(error);
       setLoading(false);
+      setError(error.message);
     }
   }
 
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
+
+  function getCurrentDate() {
+    return new Date().toLocaleDateString("en-GB", {
+      weekday: "long",
+      day: "numeric",
+    });
+  }
+
+  if (error) {
+    return <div>Error occurred in retrieving data. </div>;
+  }
+
+  useEffect(() => {
+    fetchWeatherData("London");
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <Search
         search={search}
         setSearch={setSearch}
         handleSearch={handleSearch}
       />
-      Weather
+      <div className="weather-info">
+        {loading && <p>Loading data...</p>}
+        <p>{`${getCurrentDate()}th`}</p>
+        <h1 className="">{weatherData?.name}</h1>
+
+        <h2>Description</h2>
+      </div>
+      {/* Icon */}
+      <div>
+        <h1>Temperature</h1>
+      </div>
     </div>
+    //
+    // Weather for the next 4 days
   );
 }
